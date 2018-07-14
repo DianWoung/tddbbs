7ec531aa59fad3a3a5f5e5613a758a64979b892e
dianwang <dian126@foxmail.com>
2018-07-14 16:22:28 +0800

    22,个人中心的基本实现

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,6 @@ Route::get('threads/{channel}','ThreadController@index');
 Route::post('/threads/{channel}/{thread}/replies','ReplyController@store')->name('threads.reply.store');
 
 Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
+
+//用户模型
+Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
