c5f2052bd162f54313c5c4fa883d6d6fe76c5ec8
dianwang <dian126@foxmail.com>
2018-07-16 17:09:53 +0800

    27，查询与控制器分离

--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -6,7 +6,7 @@ use App\Activity;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Database\Eloquent\Model;
 
@@ -59,4 +59,29 @@ class ActivityTest extends TestCase
 
         $this->assertEquals(2,Activity::count());
     }
+
+    /** @test */
+    public function it_fetches_a_feed_for_any_user()
+    {
+        // Given we have a thread
+        $this->signIn();
+
+        create('App\Thread', ['user_id' => auth()->id()], 2);
+
+        auth()->user()->activity()->first()
+            ->update(['created_at' => Carbon::now()->subWeek()]);
+
+        // When we fetch their feed
+        $feed = Activity::feed(auth()->user());
+
+        // Then,it should be returned in the proper format.
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->format('Y-m-d')
+        ));
+
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->subWeek()->format('Y-m-d')
+        ));
+    }
+
 }
