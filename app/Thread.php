1e53ccea63f18e773f75b2e6f34904cb29b085c8
dianwang <dian126@foxmail.com>
2018-07-30 15:58:07 +0800

    48,跟踪话题的更新

--- app/Thread.php
+++ app/Thread.php
@@ -109,4 +109,11 @@ class Thread extends Model
                     ->exists();
     }
 
+    public function hasUpdatesFor(User $user)
+    {
+        $key = $user->visitedThreadCacheKey($this);
+
+        return $this->updated_at > cache($key);
+    }
+
 }
--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -73,4 +73,20 @@ class ThreadTest extends TestCase
         $this->thread->unsubscribe($userID);
         $this->assertCount(0, $this->thread->subscriptions);
     }
+
+     /** @test */
+    public function a_thread_can_check_if_the_authenticated_user_has_read_all_replies()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        tap(auth()->user(),function ($user) use ($thread){
+            $this->assertTrue($thread->hasUpdatesFor($user));
+
+            $user->read($thread);
+
+            $this->assertFalse($thread->hasUpdatesFor($user));
+        });
+    }
 }
